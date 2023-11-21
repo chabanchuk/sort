6e7ca46089f8a06713f8f5c8a98f75f3871ed4d1
chabanchuk <145770156+chabanchuk@users.noreply.github.com>
2023-11-21 21:11:27 +0200

    Update sort.py

--- sort.py
+++ sort.py
@@ -5,6 +5,18 @@ import re
 import shutil
 
 FORBIDDEN_DIRS = []
+documents = []
+documents_ext = []
+images = []
+images_ext = []
+video = []
+video_ext = []
+audio = []
+audio_ext = []
+archives = []
+archives_ext = []
+dont_now = []
+dont_now_ext = []
 
 categories = {
     'images': ('.jpeg', '.png', '.jpg', '.svg'),
@@ -42,6 +54,7 @@ def normalize(file_name):
     return result_name
 
 def move_files(path):
+
     for root, dirs, files in os.walk(path):
         for file in files:
             file_path = os.path.join(root, file)
@@ -53,41 +66,70 @@ def move_files(path):
             move_files(os.path.join(root, dir))
 
 def remove_empty_folders(path):
-        walk = list(os.walk(path))
-        for path, _, _ in walk[::-1]:
-            if len(os.listdir(path)) == 0 and path not in FORBIDDEN_DIRS:
-                os.rmdir(path)
 
+    walk = list(os.walk(path))
+    for path, _, _ in walk[::-1]:
+        if len(os.listdir(path)) == 0 and path not in FORBIDDEN_DIRS:
+            os.rmdir(path)
 
 
 def sort_files(path):
-        for file in os.listdir(path):
-            if os.path.isfile(os.path.join(path, file)):
-                ext = os.path.splitext(file)[1]
-                if ext.lower() in categories['documents']:
-                    file_path = os.path.join(path, 'documents')
-                    shutil.move(os.path.join(path, file), os.path.join(file_path, file))
-                elif ext.lower() in categories['images']:
-                    file_path = os.path.join(path, 'images')
-                    shutil.move(os.path.join(path, file), os.path.join(file_path, file))
-                elif ext.lower() in categories['video']:
-                    file_path = os.path.join(path, 'video')
-                    shutil.move(os.path.join(path, file), os.path.join(file_path, file))
-                elif ext.lower() in categories['audio']:
-                    file_path = os.path.join(path, 'audio')
-                    shutil.move(os.path.join(path, file), os.path.join(file_path, file))        
-                elif ext.lower() in categories['archives']:
-                    file_path = os.path.join(path, 'archives')
-                    shutil.move(os.path.join(path, file), os.path.join(file_path, file))
-                    archive_path = os.path.join(file_path, os.path.splitext(file)[0])
-                    os.makedirs(archive_path, exist_ok=True)
-                    try:
-                         shutil.unpack_archive(os.path.join(file_path, file), archive_path)
-                    except shutil.ReadError as err:
-                        print(f"Не вдалося розархівувати файл {archive_path}: {err}")                    
-                else:
-                    file_path = os.path.join(path, 'dont_now_ext')
-                    shutil.move(os.path.join(path, file), os.path.join(file_path, file))
+
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            ext = os.path.splitext(file)[1]
+            if ext.lower() in categories['documents']:
+                documents.append(file)
+                documents_ext.append(ext.lower())
+                file_path = os.path.join(path, 'documents')
+                shutil.move(os.path.join(path, file), os.path.join(file_path, file))
+            elif ext.lower() in categories['images']:
+                images.append(file)
+                images_ext.append(ext.lower())
+                file_path = os.path.join(path, 'images')
+                shutil.move(os.path.join(path, file), os.path.join(file_path, file))
+            elif ext.lower() in categories['video']:
+                video.append(file)
+                video_ext.append(ext.lower())
+                file_path = os.path.join(path, 'video')
+                shutil.move(os.path.join(path, file), os.path.join(file_path, file))
+            elif ext.lower() in categories['audio']:
+                audio.append(file)
+                audio_ext.append(ext.lower())
+                file_path = os.path.join(path, 'audio')
+                shutil.move(os.path.join(path, file), os.path.join(file_path, file))        
+            elif ext.lower() in categories['archives']:
+                archives.append(file)
+                archives_ext.append(ext.lower())
+                file_path = os.path.join(path, 'archives')
+                shutil.move(os.path.join(path, file), os.path.join(file_path, file))
+                archive_path = os.path.join(file_path, os.path.splitext(file)[0])
+                os.makedirs(archive_path, exist_ok=True)
+                try:
+                        shutil.unpack_archive(os.path.join(file_path, file), archive_path)
+                except shutil.ReadError as err:
+                    print(f"Не вдалося розархівувати файл {archive_path}: {err}")                    
+            else:
+                dont_now.append(file)
+                dont_now_ext.append(ext.lower())
+                file_path = os.path.join(path, 'dont_now_ext')
+                shutil.move(os.path.join(path, file), os.path.join(file_path, file))
+
+    print(f'***********З В І Т  про  Р Е З У Л Ь Т А Т И  роботи  С К Р И П Т А ********')
+    print(f'Список файлів в категорії - Документи: \n{documents}')
+    print(f'та розширення файлів документів: \n{set(documents_ext)}')
+    print(f'Список файлів в категорії - Фото: \n{images}')
+    print(f'та розширення фотофайлів: \n{set(images_ext)}')
+    print(f'Список файлів в категорії - Відео: \n{video}')
+    print(f'та розширення видеофайлів: \n{set(video_ext)}')
+    print(f'Список файлів в категорії - Аудіо: \n{audio}')
+    print(f'та розширення аудіофайлів: {set(audio_ext)}')
+    print(f'Список файлів в категорії - Архиви: \n{archives}')
+    print(f'та розширення архивів: \n{set(archives_ext)}')
+    print(f'Список файлів в категорії - Файли з невідомим розширенням: \n{dont_now}')
+    print(f'та їх розширення, які невідомі Скрипту: \n{set(dont_now_ext)}')
+    print(f'Всі відомі Скрипту розширення знайдені ним у цільовій папці: \n{set(documents_ext)^set(images_ext)^set(video_ext)^set(audio_ext)^set(archives_ext)}')
+    print(f'Дякую за увагу та допомогу!!!')
 
 
 def main():
@@ -106,7 +148,6 @@ def main():
             print(f'Usage: {sys.argv[0]} Pyth ')
             sys.exit(1)
 
-
         
     move_files(list_dir)
 
@@ -116,5 +157,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    
